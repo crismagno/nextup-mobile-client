3bdef5e946b6e23aaa3b0aed0f9866fc2429ce35
cristhofer <cristhofer.mendonca@gmail.com>
2020-10-25 14:52:25 -0300

    Init component trending

---
+++ nextup_client/src/app/trending/components/ContainerCenter.js
@@ -0,0 +1,243 @@
+import React, { useState } from 'react';
+import {
+    StyleSheet,
+    View,
+    Text,
+    TouchableOpacity,
+    Dimensions,
+    ScrollView,
+    Image
+} from 'react-native';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
+const { width: WIDTH, height: HEIGHT } = Dimensions.get('window')
+
+const listCategoriesTest = [
+    [{
+        id: Math.random(),
+        title: 'Thai Thai',
+        stars: 4.6,
+        category: 'Restaurants',
+        img: 'url-img'
+    },
+    {
+        id: Math.random(),
+        title: 'Thai2 Thai2',
+        stars: 4.6,
+        category: 'Restaurants',
+        img: 'url-img'
+    },
+    {
+        id: Math.random(),
+        title: 'Thai3 Thai4',
+        stars: 4.6,
+        category: 'Restaurants',
+        img: 'url-img'
+    }],
+    [{
+
+        id: Math.random(),
+        title: 'Thai Thai',
+        stars: 4.6,
+        category: 'Restaurants',
+        img: 'url-img'
+    },
+    {
+        id: Math.random(),
+        title: 'Thai2 Thai2',
+        stars: 4.6,
+        category: 'Restaurants',
+        img: 'url-img'
+    },
+    {
+        id: Math.random(),
+        title: 'Thai3 Thai4',
+        stars: 4.6,
+        category: 'Restaurants',
+        img: 'url-img'
+    }],
+    [{
+        id: Math.random(),
+        title: 'Thai Thai',
+        stars: 4.6,
+        category: 'Restaurants',
+        img: 'url-img'
+    },
+    {
+        id: Math.random(),
+        title: 'Thai2 Thai2',
+        stars: 4.6,
+        category: 'Restaurants',
+        img: 'url-img'
+    },
+    {
+        id: Math.random(),
+        title: 'Thai3 Thai4',
+        stars: 4.6,
+        category: 'Restaurants',
+        img: 'url-img'
+    }],
+    [{
+        id: Math.random(),
+        title: 'Thai Thai',
+        stars: 4.6,
+        category: 'Restaurants',
+        img: 'url-img'
+    },
+    {
+        id: Math.random(),
+        title: 'Thai2 Thai2',
+        stars: 4.6,
+        category: 'Restaurants',
+        img: 'url-img'
+    },
+    {
+        id: Math.random(),
+        title: 'Thai3 Thai4',
+        stars: 4.6,
+        category: 'Restaurants',
+        img: 'url-img'
+    }]
+]
+export default props => {
+
+    const [listCategories, setListCategories] = useState(listCategoriesTest)
+
+    const renderCategory = category => {
+        return <View key={category.id}
+            style={styles.boxCategory}>
+            <Image style={styles.imgCategory}
+                imageStyle={{ borderTopRightRadius: 10, borderTopLeftRadius: 30, }}
+                source={require('./../../../assets/images/recomended1.png')} />
+            <View style={styles.boxCategoryBottom}>
+                <Text style={styles.textFont(12.5, "#fff")}>{category.category}</Text>
+            </View>
+        </View>
+    }
+
+    const renderListCategories = item => {
+        return <View style={styles.boxScrollCategory} key={item[0].id}>
+            <View style={{flex: 1}}></View>
+            <View style={{flex: 3}}>
+                <ScrollView
+                    contentContainerStyle={styles.styleScrollView2}
+                    horizontal={true}
+                    showsHorizontalScrollIndicator={false}
+                >
+                    {item.map(category => renderCategory(category))}
+                </ScrollView>
+            </View>
+        </View>
+    }
+
+    return (
+        <View style={styles.container}>
+            <ScrollView 
+                contentContainerStyle={styles.styleScrollView1}
+                horizontal={false}
+                showsVerticalScrollIndicator={false}
+            >
+                {listCategories.map(item => renderListCategories(item))}
+            </ScrollView>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        paddingVertical: 5
+    },
+
+    containerTop: {
+        height: 30,
+        flexDirection: 'row',
+        alignItems: 'center',
+        paddingHorizontal: 20,
+    },
+
+    containerCenter: {
+        flex: 1,
+        paddingHorizontal: 20,
+        paddingVertical: 5
+    },
+
+    textFont(fts, color, fontWeight) {
+        return {
+            color: color || '#FFF',
+            fontSize: fts || 13,
+            fontWeight: fontWeight && 'bold',
+        }
+    },
+
+    // parts posts
+    postContainer: {
+        // borderWidth: 1,
+        // borderColor: '#FFF',
+        marginRight: 20,
+        borderRadius: 40,
+        width: 120
+    },
+
+    postInto: {
+        flex: 1,
+        justifyContent: 'flex-end',
+        backgroundColor:'#EA909A',
+        paddingVertical: 5,
+        borderRadius: 40,
+        
+    },
+
+    postContent: {
+        // borderWidth: 1,
+        // borderColor: '#FFF',
+        height: 35,
+        paddingHorizontal: 10
+    },
+    
+    styleScrollView1: {
+        // flex: 1,
+        // borderWidth: 1,
+        // borderColor: '#FFF'
+    },
+
+    styleScrollView2: {
+        // flex: 1,
+        alignItems: 'flex-end',
+        // borderWidth: 1,
+        // borderColor: 'red',
+        paddingHorizontal: 20
+    },
+
+    boxScrollCategory: {
+        justifyContent:  'flex-end',
+        // borderWidth: 1,
+        // borderColor: '#FFF',
+        height: HEIGHT / 3,
+    },
+    boxCategory: {
+        width: 170,
+        borderWidth: 1,
+        borderColor: '#241332',
+        // height: HEIGHT / 3,
+        borderRadius: 10,
+        borderTopLeftRadius: 30,
+        borderTopRightRadius: 20,
+    },
+    imgCategory: {
+        flex: 2,
+        width: 190,
+        // height: 50,
+        borderTopLeftRadius: 30,
+        borderTopRightRadius: 10,
+    },
+    boxCategoryBottom: {
+        width: 190,
+        height: 55,
+        backgroundColor: "#352641",
+        borderBottomRightRadius: 10,
+        borderBottomLeftRadius: 10,
+        padding: 15 
+    }
+
+
+})
---
+++ nextup_client/src/app/trending/components/ContainerTop.js
@@ -0,0 +1,95 @@
+import React, { useState } from 'react';
+import {
+    StyleSheet,
+    View,
+    Text,
+    TouchableOpacity,
+    Dimensions,
+    Button,
+    SafeAreaView,
+    TextInput
+} from 'react-native';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
+import AntDesign from 'react-native-vector-icons/AntDesign'
+const { width: WIDTH, height: HEIGHT } = Dimensions.get('window')
+
+export default props => {
+
+    return (
+        <View style={styles.container}>
+            <View style={styles.containerTop}>
+                <TouchableOpacity onPress={() => props.navigation.goBack()}>
+                    <MaterialCommunityIcons style={{marginTop: 20}} name="arrow-left" color="#000" size={25} />      
+                </TouchableOpacity>      
+            </View>
+            <View style={styles.containerCenter}>
+                <View style={styles.viewBtn}>
+                    <TouchableOpacity style={styles.btnDefault}>
+                        <MaterialCommunityIcons name="heart-outline" color="#0004" size={20} />
+                    </TouchableOpacity>
+                    <Text style={styles.textFormat(11, '#0004')}>FAVOURITE</Text>
+                </View>
+                <View style={styles.viewBtn}>
+                    <TouchableOpacity style={[styles.btnDefault, { borderColor: '#D47FA9'}]}>
+                        <MaterialCommunityIcons name="trending-up" color="#000" size={25} />
+                    </TouchableOpacity>
+                    <Text style={styles.textFormat(11, '#000')}>TRENDING</Text>
+                </View>
+                <View style={styles.viewBtn}>
+                    <TouchableOpacity style={styles.btnDefault}>
+                        <AntDesign name="search1" color="#0004" size={20} />
+                    </TouchableOpacity>
+                    <Text style={styles.textFormat(11, '#0004')}>SEARCH</Text>
+                </View>
+            </View>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+
+    containerTop: {
+        flex: 1,
+		flexDirection: 'row',
+        justifyContent: 'space-between',
+        paddingHorizontal: 20,
+        paddingTop: 5
+        // borderWidth: 1
+    },
+
+    containerCenter: {
+        flex: 4,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        paddingHorizontal: 40
+    },
+
+    viewBtn: {
+        flex: 1,
+        // borderWidth: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+
+    btnDefault: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderWidth: 1,
+        borderRadius: 50,
+        width: 45,
+        height: 45,
+        marginBottom: 5,
+        borderColor: '#0004'
+    },
+
+    textFormat(fts, color) {
+        return {
+            fontSize: fts || 10,
+            color: color || '#000'
+        }
+    }
+
+})
