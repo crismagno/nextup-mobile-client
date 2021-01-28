d34299965f3cfa81b7da4b1dc1c9d5af9ca32984
Cristhofer Magno <cristhofer.mendonca@gmail.com>
2021-01-27 23:05:01 -0300

    add file menu drawerCustom

---
+++ nextup_client/src/navigation/menu/menu.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import Feather from 'react-native-vector-icons/Feather'
+
+const menu = [
+    {
+        icon: (color) => <Feather name="home" size={18} color={color} />,
+        route: 'Home',
+        label: 'Home'
+    },
+    {
+        icon: (color) => <Feather name="users" size={18} color={color} />,
+        route: 'Category',
+        label: 'Categories'
+    },
+    {
+        icon: (color) => <Feather name="calendar" size={18} color={color} />,
+        route: 'Trending',
+        label: 'My queue'
+    },
+    {
+        icon: (color) => <Feather name="user" size={18} color={color} />,
+        route: 'Contact',
+        label: 'Contact Us'
+    },
+    {
+        icon: (color) => <Feather name="info" size={18} color={color} />,
+        route: 'About',
+        label: 'About Us'
+    }
+]
+
+export default menu
